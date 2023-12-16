17446608c1207c654e41480c022ccb9fe263786f
vishnu <vishnu@gmail.com>
2023-12-16 15:31:39 +0530

    comiting changes

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Robert
+PERSON2=Raheem
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1, very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1, How are you"
+echo "$person1: whar r doing $person2, hope"
