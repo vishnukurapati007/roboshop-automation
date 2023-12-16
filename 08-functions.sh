16194d77b1bff019e14ddc1fb4811e7af4787de0
vishnu <vishnu@gmail.com>
2023-12-16 23:21:49 +0530

    iam executing mysql script

--- 08-functions.sh
+++ 08-functions.sh
@@ -18,13 +18,13 @@ VALIDATE(){
     fi
 }
 
-if [ $ID -ne 0 ]
+    if [ $ID -ne 0 ]
     then
         echo "ERROR:: Please run this script with root access"
         exit 1 # you can give other than 0
     else
     echo "You are root user"
-fi # fi means reverse of if, indicating condition end
+    fi # fi means reverse of if, indicating condition end
 
 dnf install mysql -y
 
