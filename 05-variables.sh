8656ab9f846d916a407523435791d11bbac63abb
vishnu <vishnu@gmail.com>
2023-12-16 17:01:23 +0530

    comiting changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,7 +4,7 @@ echo "Please enter your username:: "
 
 read -s USERNAME #the value entered above will be automatically atteched to USERNAME variable
 
-cho "Please enter your Password:: "
+echo "Please enter your Password:: "
 read -s PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD" # I am priting just for validation, you should not print username and passwords in scripts
