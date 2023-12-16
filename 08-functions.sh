371ef48dde02e54c0d62b820c20f24677793a9d7
vishnu <vishnu@gmail.com>
2023-12-16 23:12:32 +0530

    iam executing mysql script

---
+++ 08-functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ] 
+    then
+        echo "error installing $1 is failed"
+        exit 1
+    else
+        echo "$1 installation sucess"
+    if [ $2 -ne 0 ]
+    then
+        echo "error installing $2 is failed"
+        exit 1
+    else
+        echo "$2 installation sucess"
+    fi     
+
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf install mysql -y
+
+VALIDATE $? "installing mysql"
+
+dnf install git -y
+
+VALIDATE $? "installing git"
+
