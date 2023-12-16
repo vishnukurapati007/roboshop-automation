7d2b4b6b1aa2a3cd521f09a14bfb22ab6e33eca8
vishnu <vishnu@gmail.com>
2023-12-16 23:20:37 +0530

    iam executing mysql script

--- 08-functions.sh
+++ 08-functions.sh
@@ -15,13 +15,14 @@ VALIDATE(){
         exit 1
     else
         echo "$2 installation sucess"
-    fi }
+    fi
+}
 
 if [ $ID -ne 0 ]
-then
-    echo "ERROR:: Please run this script with root access"
-    exit 1 # you can give other than 0
-else
+    then
+        echo "ERROR:: Please run this script with root access"
+        exit 1 # you can give other than 0
+    else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
