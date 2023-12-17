d712918a0eb7b9ca063fa99e1fadfc67f8132970
vishnu <vishnu@gmail.com>
2023-12-17 19:47:57 +0530

    iam executing mysql script

--- 10-logfiles.sh
+++ 10-logfiles.sh
@@ -3,24 +3,28 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "error installing $2 is failed"
+        echo -e "error installing $2 is $R failed $N"
         exit 1
     else
-        echo "$2 installation success"
+        echo -e "$2 installation $G success $N"
 
     fi
 }
 
     if [ $ID -ne 0 ]
     then
-        echo "ERROR:: Please run this script with root access"
+        echo -e "$R ERROR:: Please run this script with root access $N"
         exit 1 # you can give other than 0
     else
-    echo "You are root user"
+    echo -e "$G You are root user $N"
     fi # fi means reverse of if, indicating condition end
 
 dnf install mysql -y &>>LOGFILE
