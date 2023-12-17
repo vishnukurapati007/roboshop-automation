496ecff05cf4cffcf784c6e62ef2849160af88f8
vishnu <vishnu@gmail.com>
2023-12-17 19:09:13 +0530

    iam executing mysql script

---
+++ 10-logfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+echo "script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "error installing $2 is failed"
+        exit 1
+    else
+        echo "$2 installation success"
+
+    fi
+}
+
+    if [ $ID -ne 0 ]
+    then
+        echo "ERROR:: Please run this script with root access"
+        exit 1 # you can give other than 0
+    else
+    echo "You are root user"
+    fi # fi means reverse of if, indicating condition end
+
+dnf install mysql -y
+
+VALIDATE $? "mysql"
+
+dnf install git -y
+
+VALIDATE $? "git"
