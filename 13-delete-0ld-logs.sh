2bb6d97d20710e5519897a8c15de45213ac2b958
vishnu <vishnu@gmail.com>
2023-12-18 22:35:09 +0530

    old logs deleting shell script

---
+++ 13-delete-0ld-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE_DIR="tmp/shellscript-logs"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR ] #!denotes oposit
+then
+    echo -e "$R source directory: $SOURCE_DIR does nor exists. $N"
+fi
+
+FILES_TO_DELETE=$( find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "deleting file: $line"
+done <<< $FILES_TO_DELETE
